f9b415338f2051bc986292a7526e028f4da83762
marat05197 <lazerwolf2007@gmail.com>
2025-10-14 15:04:32 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+#ВАРИАНТ 15 , ЗАДАНИЕ 4 (https://ege.sdamgia.ru/problem?id=99595)
+c = 0 #счетчик вариантов
+for c1 in range(1,7): # очки первого кубика
+    for c2 in range(1,7):#очки второго кубика
+        if c1+c2 == 5:#сумма очков равна 5
+            c+=1
+print(c)
