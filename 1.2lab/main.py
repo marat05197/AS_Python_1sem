67e19883caa2a80a9dd7a1935affe9782800548d
marat05197 <lazerwolf2007@gmail.com>
2025-10-21 12:08:31 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+N = int(input())
+A = float(input())
+B = float(input())
+
+H = (B - A) / N
+points = [A + i * H for i in range(N + 1)]
+
+print(H)
+for point in points:
+    print(point)
+#под переменные N A B нужно подставить свои значения
