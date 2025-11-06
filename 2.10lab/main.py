6c16d753dca0ab6515cc9503f1a434a4c30d1534
marat05197 <lazerwolf2007@gmail.com>
2025-11-06 12:33:27 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    pass
+name = input().strip()
+if not name:
+    raise Exception("Некорректное имя")
+else:
+    print("Имя введено корректно")
+
+age = int(input())
+if age < 0 or age > 120:
+    raise Exception("Некорректный возраст")
+else:
+    print("Возраст введен корректно")
+
+gender = input()
+assert gender in ['м', 'ж'], "Некорректный пол"
+print("Пол введен корректно")
