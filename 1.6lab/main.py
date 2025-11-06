b682a713179c557ee88306f6ad328276141375cd
marat05197 <lazerwolf2007@gmail.com>
2025-11-06 12:32:23 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    pass
+s = input()
+for i in range(1, len(s)):
+    if s[i].isalpha() and s[i-1].isalpha() and s[i] < s[i-1]:
+        print(i+1)
+        break
+else:
+    print(0)
