8a6c6ea8d4445bfd5dec307e2aad4203e71985fc
marat05197 <lazerwolf2007@gmail.com>
2025-11-06 12:34:22 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+d = eval(input())
+print(f"max: {max(d.values())}, min: {min(d.values())}")
+
+for key, value in d.items():
+    print(f"key: {key}, value: {value}")
+
+d2 = {'x': list(range(11, 21)), 'y': list(range(21, 31)), 'z': list(range(31, 41))}
+print(f"пятый элемент из x: {d2['x'][4]}")
+print(f"пятый элемент из y: {d2['y'][4]}")
+print(f"пятый элемент из z: {d2['z'][4]}")
