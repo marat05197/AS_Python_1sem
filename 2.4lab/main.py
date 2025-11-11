057406d45ec923a0100b5fbef11ea7a73c0ee061
marat05197 <lazerwolf2007@gmail.com>
2025-11-11 14:35:27 +0300

    Update main.py

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+def change_case(strings, to_upper=False):
+    return ' '.join(s.upper() if to_upper else s.lower() for s in strings)
+
+def filter_combined_lists(list1, list2, filter_function=None):
+    combined = list1 + list2
+    return combined if filter_function is None else list(filter(filter_function, combined))
+
+def unique_sorted_numbers(numbers):
+    return sorted(set(numbers))
+
+def strings_starting_with_upper(*args):
+    return [s for s in args if s and s[0].isupper()]
