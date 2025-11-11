2b84f714b3d4fa45936e1da5c02592bf62084be4
marat05197 <lazerwolf2007@gmail.com>
2025-11-11 14:34:41 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,16 @@
-if __name__ == "__main__":
-    pass
+def RemoveCols(A, K1, K2):
+    M = len(A)
+    if M == 0:
+        return A
+    
+    N = len(A[0])
+    if K1 > N:
+        return A
+    
+    start = K1 - 1
+    end = min(K2, N)
+    
+    for i in range(M):
+        A[i] = A[i][:start] + A[i][end:]
+    
+    return A
