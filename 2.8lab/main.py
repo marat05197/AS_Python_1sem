3a0435c37af8554db0e8b4b0521be6e970d13035
marat05197 <lazerwolf2007@gmail.com>
2025-11-25 14:09:15 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,13 @@
-if __name__ == "__main__":
-    pass
+with open('f', 'r') as file_f:
+    numbers = list(map(int, file_f.read().split()))
+
+positives = [x for x in numbers if x > 0]
+negatives = [x for x in numbers if x < 0]
+
+result = []
+for i in range(0, len(positives), 2):
+    result.extend(positives[i:i+2])
+    result.extend(negatives[i:i+2])
+
+with open('g', 'w') as file_g:
+    file_g.write(' '.join(map(str, result)))
